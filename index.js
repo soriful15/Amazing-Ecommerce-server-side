c1078f28bbc67a396faf7591dbbd1de3438f3ad3
soriful15 <sorifulislam.rs10@gmailcomm>
2023-08-26 02:34:36 +0600

    cart delete

--- index.js
+++ index.js
@@ -112,6 +112,15 @@ async function run() {
       res.send(result)
     })
 
+    app.delete('/carts/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await cartsCollection.deleteOne(query)
+      res.send(result)
+    })
+
+
+
  // jwt 
  app.post('/jwt', async (req, res) => {
   const user = req.body
